226f14665d998e4111c9f13325fc7d3a3464b821
Javokhir Isaev <javokhirisaev@usf.edu>
2025-09-28 19:45:06 -0400

    step2 done; employee array created

---
+++ cc_5.js
@@ -0,0 +1,9 @@
+//Step2
+const employees = [
+  { name: "Ahmed", hourlyRate: 20, hoursWorked: 38 },
+  { name: "Fatima", hourlyRate: 15, hoursWorked: 45 },
+  { name: "Omar", hourlyRate: 25, hoursWorked: 50 },
+  { name: "Layla", hourlyRate: 18, hoursWorked: 40 },
+  { name: "Hassan", hourlyRate: 22, hoursWorked: 60 }
+];
+
